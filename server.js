0011cf90c379ff3438250fa020e7ea573721ce95
unknown <thachnguyen281995@gmail.com>
2022-09-07 10:54:33 +0700

    upload 2

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT|| 3000
 app.use('/public',express.static(path.join(__dirname,'public')))
 app.get('/', (req, res) => {
   var duongDanFile  = path.join(__dirname,'home.html')
   res.sendFile(duongDanFile)
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
